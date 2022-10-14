2e87db1c663c979029e43386f6780cec95f1b878
Aleksei KHodakov <Heoh888@ya.ru>
2022-10-15 00:09:08 +0300

    Initial Commit

---
+++ Project shop GB/Project_shop_GBApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_shop_GBApp.swift
+//  Project shop GB
+//
+//  Created by Алексей Ходаков on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_shop_GBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
